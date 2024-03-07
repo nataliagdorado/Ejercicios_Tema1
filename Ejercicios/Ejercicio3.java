7e84b7ba396aad5e4c9dfded79edb27679df31c2
Natalia G Dorado <148120776+nataliagdorado@users.noreply.github.com>
2024-03-07 15:14:35 +0000

    ejercicio 3 hecho

--- Ejercicios/Ejercicio3.java
+++ Ejercicios/Ejercicio3.java
@@ -2,19 +2,36 @@ public class Ejercicio3 {
     public static void main(String[]args){
         int primerNumero=1;
         int ultimoNumero=100;
-        int[]multiplos getMultiplosDe5(primerNumero,ultimoNumero);
-        int suma=sumaMultiplos(multiplos);
-    }
+        int[] multiplos = getMultiplosDe5(primerNumero,ultimoNumero);
+        int suma = sumaMultiplos(multiplos);
 
-    public static int[] getMultiplosDe5(int primerNumero, int ultimoNumero){
-    // Primero cuento cuantos hay
-    int contador=0;
+        System.out.println("Multiplos de 5");
+        for (int multiple : multiplos){
+            System.out.println(multiple);
+        }
+            System.out.println("Suma de los multiplos de 5: "+suma);
+            System.out.println("Numero de multiplos de 5: " +multiplos.length); 
+
+        }
+
+        public static int[] getMultiplosDe5(int primerNumero, int ultimoNumero){
+        // Primero cuento cuantos hay
+        int contador=0;
+        for(int i=primerNumero;i<=ultimoNumero;i++){
+            if(i%5==0){
+                contador++;
+            }
+        }
+        // mi array tiene el tamaño de contador
+        int[]multiplos=new int[contador];
+    int total=0;
     for(int i=primerNumero;i<=ultimoNumero;i++){
         if(i%5==0){
-            contador++;
+            multiplos[total]=i;
+            total++;
         }
     }
-    int[]multiplos=new int[contador];
+     return multiplos;   
     
 }
 public static int sumaMultiplos(int[]array){
